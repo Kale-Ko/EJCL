61df978c346f108074df456d7da8a47532f3bae6
Kale-Ko <github@kaleko.dev>
2023-02-05 23:25:33 -0500

    Wrong visibility

--- src/main/java/io/github/kale_ko/ejcl/files/PropertiesConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/files/PropertiesConfig.java
@@ -8,13 +8,13 @@ import io.github.kale_ko.bjsl.parsers.PropertiesParser;
 public class PropertiesConfig<T> extends FileConfig<T> {
     protected BJSL<PropertiesParser> bjsl;
 
-    protected PropertiesConfig(Class<T> clazz, File file, BJSL<PropertiesParser> bjsl) {
+    public PropertiesConfig(Class<T> clazz, File file, BJSL<PropertiesParser> bjsl) {
         super(clazz, file);
 
         this.bjsl = bjsl;
     }
 
-    protected PropertiesConfig(Class<T> clazz, File file) {
+    public PropertiesConfig(Class<T> clazz, File file) {
         this(clazz, file, new BJSL<PropertiesParser>(new PropertiesParser.Builder().build()));
     }
 
