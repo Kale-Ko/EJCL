c32e34f95ee7693ca3d77ec7d2c5ab5797b6891a
Kale-Ko <github@kaleko.dev>
2023-02-09 14:42:04 -0500

    Document everything

--- src/main/java/io/github/kale_ko/ejcl/file/PropertiesConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/file/PropertiesConfig.java
@@ -5,9 +5,33 @@ import java.io.IOException;
 import io.github.kale_ko.bjsl.BJSL;
 import io.github.kale_ko.bjsl.parsers.PropertiesParser;
 
+/**
+ * A Properties File Config for storing Properties data in a File
+ *
+ * @param <T>
+ *        The type of the data being stored
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class PropertiesConfig<T> extends FileConfig<T> {
+    /**
+     * The parser/processor to use for parsing and serialization
+     * 
+     * @since 1.0.0
+     */
     protected BJSL<PropertiesParser> bjsl;
 
+    /**
+     * Create a new JsonConfig
+     *
+     * @param clazz
+     *        The class of the data being stored
+     * @param file
+     *        The file where data is being stored
+     * @param bjsl
+     *        The parser/processor to use for parsing and serialization
+     * @since 1.0.0
+     */
     public PropertiesConfig(Class<T> clazz, File file, BJSL<PropertiesParser> bjsl) {
         super(clazz, file);
 
@@ -18,10 +42,27 @@ public class PropertiesConfig<T> extends FileConfig<T> {
         this.bjsl = bjsl;
     }
 
+    /**
+     * Create a new JsonConfig
+     *
+     * @param clazz
+     *        The class of the data being stored
+     * @param file
+     *        The file where data is being stored
+     * @since 1.0.0
+     */
     public PropertiesConfig(Class<T> clazz, File file) {
         this(clazz, file, new BJSL<PropertiesParser>(new PropertiesParser.Builder().build()));
     }
 
+    /**
+     * Create a blank config file
+     * 
+     * @throws IOException
+     *         On create error
+     * @return The config bytes
+     * @since 1.0.0
+     */
     @Override
     public byte[] create() throws IOException {
         if (this.closed) {
@@ -31,6 +72,13 @@ public class PropertiesConfig<T> extends FileConfig<T> {
         return this.bjsl.emptyBytes();
     }
 
+    /**
+     * Load the config from file
+     * 
+     * @throws IOException
+     *         On load error
+     * @since 1.0.0
+     */
     @Override
     public void load() throws IOException {
         if (this.closed) {
@@ -40,11 +88,16 @@ public class PropertiesConfig<T> extends FileConfig<T> {
         this.config = this.bjsl.parse(this.loadRaw(), this.clazz);
     }
 
+    /**
+     * Save the config to bytes
+     * 
+     * @throws IOException
+     *         On save error
+     * @return The config bytes
+     * @since 1.0.0
+     */
     @Override
     public byte[] saveRaw() throws IOException {
         return this.bjsl.byteify(this.config);
     }
-
-    @Override
-    public void close() throws IOException {}
 }
--- src/main/java/io/github/kale_ko/ejcl/file/TomlConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/file/TomlConfig.java
@@ -5,9 +5,33 @@ import java.io.IOException;
 import io.github.kale_ko.bjsl.BJSL;
 import io.github.kale_ko.bjsl.parsers.TomlParser;
 
+/**
+ * A Toml File Config for storing Toml data in a File
+ *
+ * @param <T>
+ *        The type of the data being stored
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class TomlConfig<T> extends FileConfig<T> {
+    /**
+     * The parser/processor to use for parsing and serialization
+     * 
+     * @since 1.0.0
+     */
     protected BJSL<TomlParser> bjsl;
 
+    /**
+     * Create a new JsonConfig
+     *
+     * @param clazz
+     *        The class of the data being stored
+     * @param file
+     *        The file where data is being stored
+     * @param bjsl
+     *        The parser/processor to use for parsing and serialization
+     * @since 1.0.0
+     */
     public TomlConfig(Class<T> clazz, File file, BJSL<TomlParser> bjsl) {
         super(clazz, file);
 
@@ -18,19 +42,43 @@ public class TomlConfig<T> extends FileConfig<T> {
         this.bjsl = bjsl;
     }
 
+    /**
+     * Create a new JsonConfig
+     *
+     * @param clazz
+     *        The class of the data being stored
+     * @param file
+     *        The file where data is being stored
+     * @since 1.0.0
+     */
     public TomlConfig(Class<T> clazz, File file) {
         this(clazz, file, new BJSL<TomlParser>(new TomlParser.Builder().build()));
     }
 
+    /**
+     * Create a blank config file
+     * 
+     * @throws IOException
+     *         On create error
+     * @return The config bytes
+     * @since 1.0.0
+     */
     @Override
     public byte[] create() throws IOException {
         if (this.closed) {
             throw new RuntimeException("Config is already closed");
         }
 
-        return new byte[0]; // Strange behaviour using normal method
+        return this.bjsl.emptyBytes();
     }
 
+    /**
+     * Load the config from file
+     * 
+     * @throws IOException
+     *         On load error
+     * @since 1.0.0
+     */
     @Override
     public void load() throws IOException {
         if (this.closed) {
@@ -40,11 +88,16 @@ public class TomlConfig<T> extends FileConfig<T> {
         this.config = this.bjsl.parse(this.loadRaw(), this.clazz);
     }
 
+    /**
+     * Save the config to bytes
+     * 
+     * @throws IOException
+     *         On save error
+     * @return The config bytes
+     * @since 1.0.0
+     */
     @Override
     public byte[] saveRaw() throws IOException {
         return this.bjsl.byteify(this.config);
     }
-
-    @Override
-    public void close() throws IOException {}
 }
