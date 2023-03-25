eab76671654c60b06437e36d65980b2f2a3f36f1
Kale-Ko <github@kaleko.dev>
2023-03-25 15:45:50 -0400

    Add processor param back to BJSLFileConfig

--- src/main/java/io/github/kale_ko/ejcl/file/bjsl/YamlFileConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/file/bjsl/YamlFileConfig.java
@@ -2,6 +2,7 @@ package io.github.kale_ko.ejcl.file.bjsl;
 
 import java.io.File;
 import io.github.kale_ko.bjsl.parsers.YamlParser;
+import io.github.kale_ko.bjsl.processor.ObjectProcessor;
 
 /**
  * A YAML File Config for storing YAML data in a File
@@ -21,10 +22,12 @@ public class YamlFileConfig<T> extends BJSLFileConfig<T> {
      *        The file where data is being stored
      * @param parser
      *        The parser/processor to use for parsing and serialization
+     * @param processor
+     *        The ObjectProcessor to use for serialization/deserialization
      * @since 2.0.0
      */
-    public YamlFileConfig(Class<T> clazz, File file, YamlParser parser) {
-        super(clazz, file, parser);
+    public YamlFileConfig(Class<T> clazz, File file, YamlParser parser, ObjectProcessor processor) {
+        super(clazz, file, parser, processor);
     }
 
     /**
@@ -34,9 +37,11 @@ public class YamlFileConfig<T> extends BJSLFileConfig<T> {
      *        The class of the data being stored
      * @param file
      *        The file where data is being stored
-     * @since 1.0.0
+     * @param parser
+     *        The parser/processor to use for parsing and serialization
+     * @since 2.0.0
      */
-    public YamlFileConfig(Class<T> clazz, File file) {
-        this(clazz, file, new YamlParser.Builder().build());
+    public YamlFileConfig(Class<T> clazz, File file, YamlParser parser) {
+        super(clazz, file, parser, new ObjectProcessor.Builder().build());
     }
 }
