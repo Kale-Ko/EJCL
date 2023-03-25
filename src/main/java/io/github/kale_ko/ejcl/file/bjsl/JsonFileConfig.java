f80d1afc43c2ce42e51a9efd08ad4e1610d75e29
Kale-Ko <github@kaleko.dev>
2023-03-25 15:45:50 -0400

    Add processor param back to BJSLFileConfig

--- src/main/java/io/github/kale_ko/ejcl/file/bjsl/JsonFileConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/file/bjsl/JsonFileConfig.java
@@ -2,6 +2,7 @@ package io.github.kale_ko.ejcl.file.bjsl;
 
 import java.io.File;
 import io.github.kale_ko.bjsl.parsers.JsonParser;
+import io.github.kale_ko.bjsl.processor.ObjectProcessor;
 
 /**
  * A JSON File Config for storing JSON data in a File
@@ -21,10 +22,27 @@ public class JsonFileConfig<T> extends BJSLFileConfig<T> {
      *        The file where data is being stored
      * @param parser
      *        The parser/processor to use for parsing and serialization
+     * @param processor
+     *        The ObjectProcessor to use for serialization/deserialization
+     * @since 2.0.0
+     */
+    public JsonFileConfig(Class<T> clazz, File file, JsonParser parser, ObjectProcessor processor) {
+        super(clazz, file, parser, processor);
+    }
+
+    /**
+     * Create a new JsonFileConfig
+     *
+     * @param clazz
+     *        The class of the data being stored
+     * @param file
+     *        The file where data is being stored
+     * @param parser
+     *        The parser/processor to use for parsing and serialization
      * @since 2.0.0
      */
     public JsonFileConfig(Class<T> clazz, File file, JsonParser parser) {
-        super(clazz, file, parser);
+        super(clazz, file, parser, new ObjectProcessor.Builder().build());
     }
 
     /**
