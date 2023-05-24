b3636999922a7dd235ea779363a6a2627b45221b
Kale-Ko <github@kaleko.dev>
2023-05-23 21:58:36 -0400

    Split configs into Structured (Using objects) and Unstructured (Using key/value) types

--- src/main/java/io/github/kale_ko/ejcl/memory/package-info.java
+++ src/main/java/io/github/kale_ko/ejcl/memory/package-info.java
@@ -1,7 +1,8 @@
 /**
  * Contains all the classes used for Memory configs
  *
- * @see MemoryConfig
+ * @see StructuredMemoryConfig
+ * @see UnstructuredMemoryConfig
  * @since 2.0.0
  */
 package io.github.kale_ko.ejcl.memory;
