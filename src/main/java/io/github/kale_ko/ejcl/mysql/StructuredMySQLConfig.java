8767f7ed40646d6503a479cc174a7aaba94a1217
Kale-Ko <github@kaleko.dev>
2023-09-18 16:22:57 -0400

    Remove autoreconnect

--- src/main/java/io/github/kale_ko/ejcl/mysql/StructuredMySQLConfig.java
+++ src/main/java/io/github/kale_ko/ejcl/mysql/StructuredMySQLConfig.java
@@ -285,9 +285,6 @@ public class StructuredMySQLConfig<T> extends StructuredConfig<T> {
                     properties.put("password", this.password);
                 }
             }
-            properties.put("autoReconnect", true);
-            properties.put("createDatabaseIfNotExist", true);
-            properties.put("allowNanAndInf", true);
 
             this.connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, properties);
 
